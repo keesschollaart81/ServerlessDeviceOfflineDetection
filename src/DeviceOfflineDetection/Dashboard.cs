5a98b77ee90624dd7be97dbc6bb491998c4a3f40
Kees Schollaart <keesschollaart81@hotmail.com>
2019-10-27 15:59:21 +0100

    zip file & initial dashboard

--- src/DeviceOfflineDetection/Dashboard.cs
+++ src/DeviceOfflineDetection/Dashboard.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.SignalRService;
-using Microsoft.Extensions.Logging;
-using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace DeviceOfflineDetection
 {
